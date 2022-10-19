67ab31067b1f76db766df391f360272612b7d3fa
RonwistRay <rodrigo.gonzalez.olivares@alumnos.uta.cl>
2022-10-19 03:50:56 -0300

    ISRCM S

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+//import Sidebar from './Components/Sidebar';
+import Navbar from './Components/Navbar';
+import VerEmpleados from './pages/VerEmpleados';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path="/" exact component ={VerEmpleados} />
+      </Switch>
+    </Router>
     </div>
   );
 }
 
 export default App;
+
+// <div className="App"><Sidebar/></div>
+
+/*import CerrarSesion from './pages/CerrarSesion';
+import CrearGrupo from './pages/CrearGrupo';
+import Monitorear from './pages/Monitorear';
+
+import VerGrupos from './pages/VerGrupos'; */
